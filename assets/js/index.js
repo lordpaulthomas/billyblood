df615c12436a466980f983f0178d35278bb4a7e9
Paul Thomas <traitorpt@gmail.com>
2022-12-22 11:23:19 -0800

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const getLatestYoutube = async () => {
+  const response = await $.ajax({
+    method: 'GET',
+    url: '/api/youtube',
+  });
+  if (response.items !== undefined) {
+    const latestYouTube$ = $('<iframe>');
+    latestYouTube$.attr(
+      'src',
+      'https://www.youtube.com/embed/' + response.items[0].id.videoId
+    );
+    latestYouTube$.attr('height', '300px');
+    latestYouTube$.attr('width', '500px');
+    latestYouTube$.attr('allowfullscreen', true);
+    latestYouTube$.attr('style', 'border: none;');
+    latestYouTube$.attr('class', 'youtube-box');
+    $('#youtube-box').append(latestYouTube$);
+    $('#youtube-box').attr('style', 'visibility: show;');
+  } else {
+    const latestYouTube$ = $('<iframe>');
+    latestYouTube$.attr('src', 'https://www.youtube.com/embed/GoFOuKRaF7A');
+    latestYouTube$.attr('height', '300px');
+    latestYouTube$.attr('width', '500px');
+    latestYouTube$.attr('allowfullscreen', true);
+    latestYouTube$.attr('style', 'border: none;');
+    latestYouTube$.attr('class', 'youtube-box');
+    $('#youtube-box').append(latestYouTube$);
+    $('#youtube-box').attr('style', 'visibility: show;');
+  }
+};
+
+getLatestYoutube();
